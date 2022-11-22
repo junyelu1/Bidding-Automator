227658503f2cb2cdd65db213499d5526fb4f3cf0
Junye Lu <junyelu@Junyes-MBP.lan>
2022-11-22 15:49:42 +0800

    Add DownloadECPFile function Part 1, including Opening bidSection

--- extract.py
+++ extract.py
@@ -1,5 +1,41 @@
 import pandas as pd
 import numpy as np
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+import time
+
+
+def downloadECPFile(searchTerm="协议库存"):
+
+    # Initiate Google Chrome Driver
+    service = ChromeService(executable_path=ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+
+    try:
+        # Open ECP2.0 Website and Manuveur to Relevant Pages
+        driver.get('https://ecp.sgcc.com.cn/ecp2.0/portal/#/')
+        time.sleep(5)
+
+        # Subject to change if they changed the website
+        bidSection = driver.find_element(
+            By.XPATH, "/html/body/app-root/app-main/app-nav/div/div/ul/li[2]/ul/li[2]/a")
+        bidSectionLink = bidSection.get_attribute("href")
+        driver.get(bidSectionLink)
+        time.sleep(3)
+
+    except:
+        return "ECP2.0 Website Layout Mightbe changed, Examine XPATH"
+
+    try:
+        # Search relevant keys
+        searchBox = driver.find_element(By.NAME, "key")
+        searchBox.send_keys()
+        searchBox.submit()
+        time.sleep(1)
+
+        # If result not found, return error
 
 
 def filterDownloadExcel(downloadPath: str, savePath='~/Desktop/相关清单/'):
